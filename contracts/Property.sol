26e4ddb4a4fe7fdf1cbcd6f4ecfebfb094b3e389
Stephanie Fay <stephaniemfay@gmail.com>
2019-10-25 01:58:01 -0300

    Smoke test

---
+++ contracts/Property.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Property {
+	// Store property
+	// Read property
+	string public property;
+	// constructor
+
+	constructor () public {
+		property = "Imóvel 1";
+	}
+}
